83b6ec0873c7ffbd68d5630baca23a6264952901
Anderson Marques <andersongabriel96.ag@gmail.com>
2018-04-18 10:14:47 -0300

    Criado tratamento padrão para outros erros

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpEvent, HttpHandler, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
 
-    constructor(public storage : StorageService){}
+    constructor(public storage : StorageService, public alertController : AlertController){}
 
     intercept(req : HttpRequest<any>, next : HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).catch((error, caught) => {
@@ -27,9 +28,16 @@ export class ErrorInterceptor implements HttpInterceptor{
 
             //Trata os erros de acordo com o tipo
             switch(erroObj.status) {
+                case 401:
+                    this.handle401();
+                    break;
+
                 case 403 :
                     this.handle403();
                     break;
+
+                default :
+                    this.handleDefaultError(erroObj);
             }   
 
             return Observable.throw(error);
@@ -41,6 +49,31 @@ export class ErrorInterceptor implements HttpInterceptor{
         this.storage.setLocalUser(null);
     }
 
+    handle401() {
+        let alert = this.alertController.create({
+            title: 'Erro 401: Falha de autenticação',
+            message: 'Email ou Senha incorretos',
+            //Essa parâmetro serve para desabilitar o alert apenas se o botão do mesmo for clicado.
+            enableBackdropDismiss : false,
+            buttons: [
+                { text:'OK' }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefaultError(erroObj) {
+        let alert = this.alertController.create({
+            title: 'Erro '+erroObj.status+': '+erroObj.error,
+            message: erroObj.message,
+            //Essa parâmetro serve para desabilitar o alert apenas se o botão do mesmo for clicado.
+            enableBackdropDismiss: false,
+            buttons: [
+                { text: 'OK' }
+            ]
+        });
+        alert.present();
+    }
 
 }
 
