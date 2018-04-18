07139ab2880a19b2fc4bc7bc68f5cd2d6603cd23
Anderson Marques <andersongabriel96.ag@gmail.com>
2018-04-18 19:11:13 -0300

    Carregando dados do produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -2,13 +2,20 @@ import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
+import { ProdutoDTO } from "../../models/produto.dto";
 
 @Injectable()
 export class ProdutoService {
      constructor(public http : HttpClient) {}
 
+     //Buscar detalhes do produto
+     findById(produto_id : string) {
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`)
+     }
+
+     //Mostrar produtos de acordo com a categoria
      findByCategoria(categoria_id : string) {
-         return this.http.get(`${API_CONFIG.baseUrl}/produtos?categoria=${categoria_id}`);
+         return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos?categoria=${categoria_id}`);
      }
 
      //Não funciona, não criei o bucket para armazenas imagens com valores padronizados, testar com o img.ur ou outro site para hospedar
