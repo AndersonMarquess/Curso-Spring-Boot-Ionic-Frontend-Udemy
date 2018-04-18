43f52debbcbb1c863fdb90c17dd6abaaf800f006
Anderson Marques <andersongabriel96.ag@gmail.com>
2018-04-18 10:32:02 -0300

    Adicionado funcionalidade de logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Title } from '@angular/platform-browser';
+import { AuthService } from '../services/auth.service';
 
 
 @Component({
@@ -14,16 +16,17 @@ export class MyApp {
 
   pages: Array<{title: string, component: string}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
-    this.initializeApp();
+  constructor(public platform: Platform, public statusBar: StatusBar,
+     public splashScreen: SplashScreen, public auth : AuthService) {
+      this.initializeApp();
 
-    // used for an example of ngFor and navigation
-    this.pages = [
-      //Itens que aparecem no menu lateral
-      { title: 'Profile', component: "ProfilePage" },
-      { title: 'Categorias', component: "CategoriasPage" }
-    ];
-
+      // used for an example of ngFor and navigation
+      this.pages = [
+        //Itens que aparecem no menu lateral
+        { title: 'Profile', component: "ProfilePage" },
+        { title: 'Categorias', component: "CategoriasPage" },
+        { title: 'Logout', component: "" }
+      ];
   }
 
   initializeApp() {
@@ -35,9 +38,13 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  openPage(page : {title : string, component : string}) {
+    if(page.title == "Logout") {
+      this.auth.logout();
+      this.nav.setRoot("HomePage");
+    }else {
+      this.nav.setRoot(page.component);
+    }
   }
+
 }
